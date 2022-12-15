5ec6aa966519fa71c11ca4fe58ddeaaa811e6a06
Ethan <et.l.nicholas@gmail.com>
2022-12-15 13:14:30 +1100

    set up basic react and material ui for web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './shop-logo.png';
+import Product from './components/Product';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+
       </header>
+      <body>
+      < Product />
+        {/* < product name="Rainbow" description="This is a paint of Rainbow" imageUrl="https://images.unsplash.com/photo-1545231097-cbd796f1d95f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1095&q=80"/>
+        < product name="Smoke" description="This is a art work of smoke" />
+        < product name="Rainbow" description="This is a paint of Rainbow" /> */}
+      </body>
     </div>
   );
 }
